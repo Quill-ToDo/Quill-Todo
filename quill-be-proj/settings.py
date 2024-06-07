0caca1af052708af279677dac882329b9cd9c74a
Lily Davisson <lily@lilydavisson.com>
2024-06-07 13:51:47 -0700

    Upgrade django version

--- quill-be-proj/settings.py
+++ quill-be-proj/settings.py
@@ -121,19 +121,24 @@ TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
-USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
 
-REACT_APP_DIR = os.path.join(BASE_DIR, 'quill-fe')
-
-STATICFILES_DIRS = [
-    os.path.join(REACT_APP_DIR, 'build', 'static'),
-]
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+        "OPTIONS": {
+            "location": '/quill-fe/build/static',
+            "base_url": BASE_DIR,
+        }
+    },
+}
 
 # CORS
 # turn this bink off in prod
