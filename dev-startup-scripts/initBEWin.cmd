33c815698c4c457c39773ab5d8f852d68a23cbfd
Lily <lily@lilydavisson.com>
2024-06-26 22:02:29 -0700

    Update docs and startup scripts for Unix compatibility

--- dev-startup-scripts/initBEWin.cmd
+++ dev-startup-scripts/initBEWin.cmd
@@ -1,8 +1,10 @@
 :: Run me once to set you up for back-end work on Windows :)
-workon quill 
+workon quill-venv
 pip install -r requirements.txt 
 pg_ctl start 
 chcp 1252 
+cd dev-startup-scripts
 psql -d postgres -a -f setup.sql 
+cd ..
 py manage.py makemigrations 
 py manage.py migrate
